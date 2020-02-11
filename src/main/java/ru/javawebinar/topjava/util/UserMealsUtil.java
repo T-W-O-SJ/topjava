86becde597d167ce7b8ccaa63654f56a36423d75
Sergey Vilkov <wic555ked@gmail.com>
2020-02-11 15:41:43 +0300

    Begin work

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,6 +6,7 @@ import ru.javawebinar.topjava.model.UserMealWithExcess;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,6 +30,13 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with excess. Implement by cycles
+        int daylyCal =0;
+        List<UserMeal> temp = new ArrayList<>();
+        for(UserMeal meal : meals){
+            daylyCal = daylyCal + meal.getCalories();
+
+        }
+
         return null;
     }
 
